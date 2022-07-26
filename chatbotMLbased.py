b6b92442c8093483702873dd076be40ccb7dbe5c
tel-chemistry <tel.chemistry@gmail.com>
2022-07-26 15:38:20 +0700

    menaikkan threshold

--- chatbotMLbased.py
+++ chatbotMLbased.py
@@ -94,17 +94,39 @@ def bag_of_words(text, vocab):
     return np.array(bow)
 
 
-def pred_class(text, vocab=words, labels=classes):
+def pred_class(text, vocab, labels):
     bow = bag_of_words(text, vocab)
     result = model.predict(np.array([bow]))[0]
-    thresh = 0.2
+
+    # melakukan iterasi dari seluruh kemungkinan kelas
+    thresh = 0.5
     y_pred = [[idx, res] for idx, res in enumerate(result) if res > thresh]
 
     y_pred.sort(key=lambda x: x[1], reverse=True)
     return_list = []
+
     for r in y_pred:
         return_list.append(labels[r[0]])
-    return return_list
+    if (len(return_list)) == 0:
+        return ["no_response"]
+    else:
+        return return_list
+
+def pred_class_ml(text, vocab, labels):
+    bow = bag_of_words(text, vocab)
+    result = model_ml.predict(np.array([bow]))[0]
+    thresh = 0.5
+    y_pred = [[idx, res] for idx, res in enumerate(result) if res > thresh]
+
+    y_pred.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+
+    for r in y_pred:
+        return_list.append(labels[r[0]])
+    if (len(return_list)) == 0:
+        return ["no_response"]
+    else:
+        return return_list
 
 
 def get_response(intents_list, intents_json = data):
@@ -129,7 +151,6 @@ def get_cosine(vec1, vec2):
         return 0.0
     else:
         return float(numerator) / (denominator)
-
 
 def text_to_vector(text):
     word = re.compile(r'\w+')
